f90b2351dd1658776c03f0910abf2c5371312f20
Imtiaz Evan <imtiazevan720@gmail.com>
2026-02-04 23:01:50 -0600

    Force recreate containers in cluster script

--- start_cluster.sh
+++ start_cluster.sh
@@ -19,10 +19,10 @@ CLIENT_SCALE="${CLIENT_SCALE:-40}"
 PARALLEL="${PARALLEL:-4}"
 
 # Start server first
-ssh "$SERVER_HOST" "cd '$REMOTE_DIR' && SUBSCRIBED_POWER='$SUBSCRIBED_POWER' docker compose up -d --build server"
+ssh "$SERVER_HOST" "cd '$REMOTE_DIR' && SUBSCRIBED_POWER='$SUBSCRIBED_POWER' docker compose up -d --build --force-recreate server"
 
 # Start clients on all hosts in parallel
 printf "%s\n" "${CLIENT_HOSTS[@]}" | \
   xargs -n1 -P"$PARALLEL" -I{} ssh {} "cd '$REMOTE_DIR' && \
   HPC_MANAGER_HOST='$HPC_MANAGER_HOST' HPC_MANAGER_PORT='$HPC_MANAGER_PORT' HPC_MANAGER_FLASK_SERVER_PORT='$HPC_MANAGER_FLASK_SERVER_PORT' \
-  docker compose up -d --build --no-deps --scale client=$CLIENT_SCALE client"
+  docker compose up -d --build --force-recreate --no-deps --scale client=$CLIENT_SCALE client"
